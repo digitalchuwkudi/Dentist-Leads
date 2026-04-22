a1a8658698780b423b56d558ced1642ca73d017a
digitalchuwkudi <madudimcjx@gmail.com>
2026-04-22 01:58:02 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/', // important for Cloudflare Pages
+   base: './', important for Cloudflare Pages
 
     plugins: [
       react(),
